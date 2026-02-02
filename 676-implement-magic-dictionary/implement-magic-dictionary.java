0a3103db69afa39259ed1a18324690d76d8d647f
Tanishq.S.dev05 <tanishqshukla.dev@gmail.com>
2026-02-02 21:06:08 +0530

    Time: 32 ms (45.12%) | Memory: 48.1 MB (44.05%) - LeetSync

--- 676-implement-magic-dictionary/implement-magic-dictionary.java
+++ 676-implement-magic-dictionary/implement-magic-dictionary.java
@@ -1,41 +1,67 @@
 class MagicDictionary {
-    Set<String> originalWords;
-    Map<String, Integer> extensions;
-
+    class Node{ //Trie Node
+        Node[] next;
+        boolean isEnd;
+        Node(){
+            next=new Node[26];
+            isEnd=false;
+        }
+    }
+    public void insert(Node root,String str){
+        Node cur=root;
+        for(int i=0;i<str.length();i++){
+            int c=str.charAt(i)-'a';
+            if(cur.next[c]==null){
+                Node newNode=new Node();
+                cur.next[c]=newNode;
+            }
+            cur=cur.next[c];
+            if(i==str.length()-1){
+                cur.isEnd=true;
+            }
+        }
+    }
+    Node root;
     public MagicDictionary() {
-        originalWords = new HashSet<>();
-        extensions = new HashMap<>();
+        root=new Node();
     }
     
     public void buildDict(String[] dictionary) {
-        for (String word : dictionary) {
-            originalWords.add(word);
-            char[] str = word.toCharArray();
-            int n = str.length;
-            for (int i = 0; i < n; i++) {
-                char temp = str[i];
-                str[i] = '*';
-                String key = new String(str);
-                extensions.put(key, extensions.getOrDefault(key, 0) + 1);
-                str[i] = temp;
-            }
+        for(int i=0;i<dictionary.length;i++){
+            String str=dictionary[i];
+            insert(root,str);
         }
     }
     
     public boolean search(String searchWord) {
-        char[] str = searchWord.toCharArray();
-        int n = str.length;
-        for (int i = 0; i < n; i++) {
-            char temp = str[i];
-            str[i] = '*';
-            String key = new String(str);
-            if (extensions.containsKey(key)) {
-            
-                if (extensions.get(key) >= 2 || !originalWords.contains(searchWord)) {
-                    return true;
+        boolean flag=false; //false means not modified
+        Node cur=root;
+        return dfs(searchWord,0,cur,flag);
+    }
+
+    public boolean dfs(String searchWord,int index,Node node,boolean flag){
+        if(index==searchWord.length()){
+            if(node.isEnd){
+                return flag;
+            }
+            else{
+                return false;
+            }
+        }
+        int curIdx=searchWord.charAt(index)-'a';
+        for(int i=0;i<26;i++){
+            if(node.next[i]!=null){
+                if(curIdx==i){
+                    boolean res=dfs(searchWord,index+1,node.next[i],flag);
+                    if(res) return true;
+                }
+                else{
+                    if(!flag){
+                        boolean res=dfs(searchWord,index+1,node.next[i],true);
+                        if(res) return true;
+                    }
                 }
             }
-            str[i] = temp;
         }
         return false;
     }
