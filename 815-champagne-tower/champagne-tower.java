ab68b8d40a2c759cdbad1ee4f9090a44f3017786
Tanishq.S.dev05 <tanishqshukla.dev@gmail.com>
2026-02-14 22:55:26 +0530

    Time: 1 ms (100.00%) | Memory: 46.8 MB (96.47%) - LeetSync

--- 815-champagne-tower/champagne-tower.java
+++ 815-champagne-tower/champagne-tower.java
@@ -1,18 +1,16 @@
 class Solution {
-    public double champagneTower(int poured, int query_row, int query_glass) {
-        double[][] pyramid = new double[query_row + 2][query_row + 2];
-        pyramid[0][0] = (double)poured;
-        for(int i = 0; i <= query_row; i++){
-            for(int j = 0; j <= i; j++){
-                if(pyramid[i][j] > 1.0){
-                    double remain = pyramid[i][j] - 1.0;
-                    pyramid[i + 1][j] += remain / 2;
-                    pyramid[i + 1][j + 1] += remain / 2;
-                    pyramid[i][j] = 1.0;
-                }
+    public double champagneTower(int poured, int row, int col) {
+        double[] dp = new double[col + 2];
+        dp[0] = poured;
+        for(int i = 0; i < row; i++) {
+            for(int j = Math.min(i, col); j >= 0; j--) {
+                if(dp[j] > 1) {
+                    double val = (dp[j] - 1) / 2.0;
+                    dp[j] = val;
+                    dp[j + 1] += val;
+                }else dp[j] = 0;
             }
         }
-
-        return pyramid[query_row][query_glass];
+        return Math.min(1, dp[col]);
     }
 }
